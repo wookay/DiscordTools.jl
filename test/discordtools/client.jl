8261d919711c1aaddc07a02bf5b704df9b986210
WooKyoung Noh <wookay.noh@gmail.com>
2019-11-10 18:50:10 +0900

    test handlers

--- test/discordtools/client.jl
+++ test/discordtools/client.jl
@@ -7,4 +7,18 @@ token = ""
 c = Client(token)
 @test !c.ready
 
+add_command!(c, :echo; pattern=r"^echo (.+)") do c, msg, noprefix
+    reply(c, msg, noprefix)
+end
+
+hs = get(c.handlers, Discord.MessageCreate, Dict())
+@test collect(keys(hs)) == [:echo, :DJL_DEFAULT]
+h = get(hs, :echo, nothing)
+@test h.h.predicate.pattern == r"^echo (.+)"
+
+delete_command!(c, :echo)
+
+hs = get(c.handlers, Discord.MessageCreate, Dict())
+@test collect(keys(hs)) == [:DJL_DEFAULT]
+
 end # module test_discordtools_client
